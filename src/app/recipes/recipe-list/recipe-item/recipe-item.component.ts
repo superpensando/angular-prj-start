8eb34f5f9699e5748c274aa60a2116e51ebdbbfe
Laura Rodríguez Castillo <laura.rodriguez@crealogix.com>
2023-04-19 14:36:33 +0200

    Passin data with Event+ Property Binding (combined)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 @Component({
   selector: 'app-recipe-item',
@@ -8,6 +8,12 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipeItem: Recipe;
+
+  @Output() recipeItemSelected = new EventEmitter<void>();
+
+  onSelect(recipeItemSelected:any) {
+      this.recipeItemSelected.emit();
+  }
 
   constructor() { }
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,18 +8,24 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
-    'A Test Description',
+    new Recipe('Recipe Name 1',
+    'A Test Description 1',
     'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=300,272'),
-    new Recipe('A Test Recipe',
-    'A Test Description',
+    new Recipe('Recipe Name 2',
+    'A Test Description 2',
     'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=300,272'),
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected ( recipe: Recipe ){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
