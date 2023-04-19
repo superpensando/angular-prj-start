a91f6474e7259b9cac68bbb6d15fb527eb2d06c0
Laura Rodríguez Castillo <laura.rodriguez@crealogix.com>
2023-04-19 13:15:00 +0200

    Adding navigation with Event Binding and ngif

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   collapsed = true;
+
+  @Output() sectionSelected = new EventEmitter<string>();
+
+  onSelect(section:string) {
+      this.sectionSelected.emit(section);
+  }
 
   constructor() { }
 
