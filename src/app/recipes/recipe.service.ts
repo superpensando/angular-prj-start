79180bcac396ca578020a54cd7942a8e3d5c41ca
Laura Rodríguez Castillo <laura.rodriguez@crealogix.com>
2023-05-18 22:36:25 +0200

    Adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shopping/ingredient.model';
 
 
 export class RecipeService {
@@ -8,12 +9,22 @@ export class RecipeService {
   recipeItemSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Recipe Name 1',
-    'A Test Description 1',
-    'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=300,272'),
-    new Recipe('Recipe Name 2',
-    'A Test Description 2',
-    'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=300,272'),
+    new Recipe('Tortilla de Patatas',
+    'Cómo hacer tortilla de patatas de manera fácil y con todas las fotos del paso a paso, para comer',
+    'https://cdn.elcocinerocasero.com/imagen/receta/1000/2022-06-15-19-04-10/tortilla-de-patata.jpeg',
+    [
+      new Ingredient('Patatas',8),
+      new Ingredient('huevo',5)
+    ]),
+    new Recipe('Fideos Japoneses',
+    'Un plato de estilo japones que podemos elaborar hoy para cenar',
+    'https://cdn.cookmonkeys.es/recetas/medium/yakisoba-fideos-japoneses-fritos-5810.jpeg',
+    [
+      new Ingredient('Fideos',1),
+      new Ingredient('Pimiento rojo',1),
+      new Ingredient('Pimiento verde',1),
+      new Ingredient('cebolla',1)
+    ]),
   ];
 
   getRecipes() {
